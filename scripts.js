bff2c83a13ec1e2a2c2a78ddbd8984aa59b8147d
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-07 14:28:17 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,9 @@ var canvas = document.getElementById("myCanvas")//id is in quotes
 var ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "00FFFF";
+
+//The fillRect(x,y,width,height) method draws a rectangle, filled with the fill style, on the canvas:
+//      ctx.fillRect(0, 0, 150, 75);
+
+
+ctx.fillRect(0, 0, 100, 100);
